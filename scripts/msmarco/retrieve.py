e82e97fdde9fb83d6c063148e501b663f802fa45
lintool <jimmylin@uwaterloo.ca>
2020-06-12 12:40:18 -0400

    Delint MS MARCO scripts

--- scripts/msmarco/retrieve.py
+++ scripts/msmarco/retrieve.py
@@ -24,18 +24,18 @@ from pyserini.search import SimpleSearcher
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Retrieve MS MARCO Passages.')
-    parser.add_argument('--qid_queries', required=True, default='', help='query id - query mapping file')
-    parser.add_argument('--output', required=True, default='', help='output filee')
-    parser.add_argument('--index', required=True, default='', help='index path')
-    parser.add_argument('--hits', default=10, type=int, help='number of hits to retrieve')
-    parser.add_argument('--k1', default=0.82, type=float, help='BM25 k1 parameter')
-    parser.add_argument('--b', default=0.68, type=float, help='BM25 b parameter')
+    parser.add_argument('--queries', required=True, default='', help='Queries file.')
+    parser.add_argument('--output', required=True, default='', help='Output run file.')
+    parser.add_argument('--index', required=True, default='', help='Index path.')
+    parser.add_argument('--hits', default=10, type=int, help='Number of hits to retrieve.')
+    parser.add_argument('--k1', default=0.82, type=float, help='BM25 k1 parameter.')
+    parser.add_argument('--b', default=0.68, type=float, help='BM25 b parameter.')
     # See our MS MARCO documentation to understand how these parameter values were tuned.
     parser.add_argument('--rm3', action='store_true', default=False, help='use RM3')
-    parser.add_argument('--fbTerms', default=10, type=int, help='RM3 parameter: number of expansion terms')
-    parser.add_argument('--fbDocs', default=10, type=int, help='RM3 parameter: number of documents')
-    parser.add_argument('--originalQueryWeight', default=0.5, type=float, help='RM3 parameter: weight to assign to the original query')
-    parser.add_argument('--threads', default=1, type=int, help='Maximum number of threads')
+    parser.add_argument('--fbTerms', default=10, type=int, help='RM3: number of expansion terms.')
+    parser.add_argument('--fbDocs', default=10, type=int, help='RM3: number of documents.')
+    parser.add_argument('--originalQueryWeight', default=0.5, type=float, help='RM3: weight of original query.')
+    parser.add_argument('--threads', default=1, type=int, help='Maximum number of threads.')
 
     args = parser.parse_args()
 
@@ -43,21 +43,22 @@ if __name__ == '__main__':
 
     searcher = SimpleSearcher(args.index)
     searcher.set_bm25(args.k1, args.b)
-    print('Initializing BM25, setting k1={} and b={}'.format(args.k1, args.b), flush=True)
+    print(f'Initializing BM25, setting k1={args.k1} and b={args.b}', flush=True)
     if args.rm3:
         searcher.set_rm3(args.fbTerms, args.fbDocs, args.originalQueryWeight)
-        print('Initializing RM3, setting fbTerms={}, fbDocs={} and originalQueryWeight={}'.format(args.fbTerms, args.fbDocs, args.originalQueryWeight), flush=True)
+        print(f'Initializing RM3, setting fbTerms={args.fbTerms}, fbDocs={args.fbDocs}, ' +
+              f' and originalQueryWeight={args.originalQueryWeight}', flush=True)
 
     if args.threads == 1:
 
         with open(args.output, 'w') as fout:
             start_time = time.time()
-            for line_number, line in enumerate(open(args.qid_queries, 'r', encoding='utf8')):
+            for line_number, line in enumerate(open(args.queries, 'r', encoding='utf8')):
                 qid, query = line.strip().split('\t')
                 hits = searcher.search(query, args.hits)
                 if line_number % 100 == 0:
                     time_per_query = (time.time() - start_time) / (line_number + 1)
-                    print('Retrieving query {} ({:0.3f} s/query)'.format(line_number, time_per_query), flush=True)
+                    print(f'Retrieving query {line_number} ({time_per_query:0.3f} s/query)', flush=True)
                 for rank in range(len(hits)):
                     docno = hits[rank].docid
                     fout.write('{}\t{}\t{}\n'.format(qid, docno, rank + 1))
@@ -66,7 +67,7 @@ if __name__ == '__main__':
         queries = []
         result_dict = {}
 
-        for line_number, line in enumerate(open(args.qid_queries, 'r', encoding='utf8')):
+        for line_number, line in enumerate(open(args.queries, 'r', encoding='utf8')):
             qid, query = line.strip().split('\t')
             qids.append(qid)
             queries.append(query)
@@ -78,8 +79,8 @@ if __name__ == '__main__':
                 hits = results.get(qid)
                 for rank in range(len(hits)):
                     docno = hits[rank].docid
-                    fout.write('{}\t{}\t{}\n'.format(qid, docno, rank + 1))
+                    fout.write(f'{qid}\t{docno}\t{rank+1}\n')
 
     total_time = (time.time() - total_start_time)
-    print('Total retrieval time: {:0.3f} s'.format(total_time))
+    print(f'Total retrieval time: {total_time:0.3f} s')
     print('Done!')

84f453c1af1dca2c03561b0087fd4f81f23c242e
lintool <jimmylin@uwaterloo.ca>
2020-12-04 17:50:40 -0500

    Added --quiet mode to suppress warnings.

--- scripts/msmarco/convert_trec_to_msmarco_run.py
+++ scripts/msmarco/convert_trec_to_msmarco_run.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
                         help='output MS MARCO-formatted run file')
     parser.add_argument('--k', type=int, default=-1,
                         help='Number of hits to write to the run file. Write all hits if -1.')
+    parser.add_argument('--quiet', action='store_true', help="Suppresses all warnings.")
 
     args = parser.parse_args()
 
@@ -43,25 +44,25 @@ if __name__ == '__main__':
                 n_docs = 0
 
             if last_score is not None:
-                if score == last_score:
+                if score == last_score and not args.quiet:
                     logging.warning(
                         f'Score of {score} for doc id {doc_id} is the same of doc id '
                         f'{last_doc_id} for query id {query_id}. This will likely impact metrics '
                         ' negatively.')
 
-                if rank == last_rank:
+                if rank == last_rank and not args.quiet:
                     logging.warning(
                         f'Rank of {rank} for doc id {doc_id} is the same of doc id '
                         f'{last_doc_id} for query id {query_id}. This will likely impact metrics '
                         ' negatively.')
 
-                if score > last_score:
+                if score > last_score and not args.quiet:
                     logging.warning(
                         f'Score of {score} for current doc id {doc_id} is greater than the score '
                         f'{last_score} of the previous doc id {last_doc_id} for query id '
                         f'{query_id}. This will likely impact metrics negatively.')
 
-                if rank < last_rank:
+                if rank < last_rank and not args.quiet:
                     logging.warning(
                         f'Rank of {rank} for current doc id {doc_id} is lower than the rank '
                         f'{last_rank} of the previous doc id {last_doc_id} for query id '
